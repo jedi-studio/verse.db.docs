1011dfbffe386ee82a6fb6f9ecf1d35951f3d2a6
Mark Maher <100785809+Marco5dev@users.noreply.github.com>
2024-05-01 02:22:32 +0300

    about us update

--- components/About.tsx
+++ components/About.tsx
@@ -23,75 +23,16 @@ function About() {
               Studio.
             </p>
             <p className="py-6" style={{ opacity: "0.5" }}>
-              verse.db isn't just a database, it's your universal data bridge.
-              Designed for unmatched flexibility, security, and performance,
-              verse.db empowers you to manage your data with ease.
+              <span className="font-bold">JEDI Studio</span> isn't just a team we are a big family who wantes to show off all the skills we have
+              we are sure that we can do the impossible and we wants to make the world a better place with coding from now on 
+              we are <span className="font-bold">JavaScript Experienced Developers International Studios</span> (JEDI Studio).
             </p>
-            <div className="devs">
-              <div className="title font-bold text-2xl">
-                <h2>Developers</h2>
-              </div>
-              <div className="descri">
-                <p className="" style={{ opacity: "0.5" }}>
-                  JavaScript Experienced Developers International.
-                </p>
-              </div>
-              <div className="about-cards flex flex-col items-center">
-
-                <div className="card w-85 bg-base-100 shadow-xl card-side" style={{ width: "450px" }} >
-                  <figure style={{ width: "250px", height: "180px" }}>
-                    <img
-                      src="https://marco5dev.site/assets/mark-img1-f8981f4a.jpg"
-                      alt="Mark Maher"
-                    />
-                  </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">Mark Maher</h2>
-                    <p style={{ textAlign: "left" }}>Full Stack Developer</p>
-                    <div className="card-actions flex justify-end font-bold">
-                      <a href="https://facebook.com/marco5dev" className="badge badge-info hover:badge-outline" target="_blank" >{"Facebook"}</a>
-                      <a href="https://instagram.com/marco5dev" className="badge badge-secondary hover:badge-outline" target="_blank" >{"Instagram"}</a>
-                      <a href="https://github.com/ANAS799" className="badge badge-ghost hover:badge-outline" target="_blank" >{"Github"}</a>
-                      <a href="https://twitter.com/mark5dev" className="badge badge-ghost hover:badge-outline" target="_blank" >{"X"}</a>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="card w-85 bg-base-100 shadow-xl card-side" style={{ width: "450px" }} >
-                  <figure style={{ width: "180px", height: "180px" }}>
-                    <img
-                      src="https://media.discordapp.net/attachments/1153369478860648548/1234722757753966635/shadow-boy-white-eyes-unique-cool-pfp-nft-13yuypusuweug9xn.jpg?ex=6631c4ad&is=6630732d&hm=9981acd73269cdb1ef93392d279c630ce3df357cd78821c887109625e6dbf1c6&=&format=webp&width=592&height=592"
-                      alt="Anas Khlaid"
-                    />
-                  </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">Anas Khalid</h2>
-                    <p style={{ textAlign: "left" }}>Backend Developer</p>
-                    <div className="card-actions flex justify-end font-bold">
-                      <a href="https://github.com/ANAS799" className="badge badge-ghost hover:badge-outline" target="_blank" >{"Github"}</a>
-                      <a href="https://twitter.com/an79_ii" className="badge badge-ghost hover:badge-outline" target="_blank" >{"X"}</a>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="card w-85 bg-base-100 shadow-xl card-side" style={{ width: "450px" }} >
-                  <figure style={{ width: "180px", height: "180px" }}>
-                    <img
-                      src="https://media.discordapp.net/attachments/1153369478860648548/1234722757753966635/shadow-boy-white-eyes-unique-cool-pfp-nft-13yuypusuweug9xn.jpg?ex=6631c4ad&is=6630732d&hm=9981acd73269cdb1ef93392d279c630ce3df357cd78821c887109625e6dbf1c6&=&format=webp&width=592&height=592"
-                      alt="Karem"
-                    />
-                  </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">Karem</h2>
-                    <p style={{ textAlign: "left" }}>Full Stack Web Developer</p>
-                    <div className="card-actions flex justify-end font-bold">
-                      <a href="https://github.com/kmoshax" className="badge badge-ghost hover:badge-outline" target="_blank" >{"Github"}</a>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
+            <a
+            className="btn btn-outline btn-error"
+            href="https://jedi-studio.com"
+          >
+            Team Website
+          </a>
           </div>
         </div>
       </div>
